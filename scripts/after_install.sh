1331655307d7ff4cc9ebdd9036dc220ffbc715ea
15HashBrown <116076902+15HashBrown@users.noreply.github.com>
2023-05-05 16:12:36 +0800

    Delete scripts directory

--- scripts/after_install.sh
+++
@@ -1,7 +0,0 @@
-#EC2_INSTANCE_ID=i-030e7ac2ee86a7448
-#EC2_AZ=us-east-2c
-#sudo sed -i "s/was deployed/was deployed on $EC2_INSTSNCE_ID in $EC2_AZ/g" /var/www/html/index.html
-
-#關閉../scripts目錄下檔案的一般瀏覽權限，限制瀏覽
-sudo chmod 600 /var/www/html/scripts/*
-
